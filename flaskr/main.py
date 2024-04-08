439688309e3fa8f81adaecba9d5af0923b15df61
yohichi <s2210653@u.tsukuba.ac.jp>
2024-04-08 14:56:50 +0900

    Initial commit

---
+++ flaskr/main.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "<p>index</p>"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=80)
