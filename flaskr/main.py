63e413e8073ee655764cf1e08eeeeb5056dfc90d
yohichi <s2210653@u.tsukuba.ac.jp>
2024-04-08 15:53:12 +0900

    Add temlates

--- flaskr/main.py
+++ flaskr/main.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "<p>index</p>"
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=80)
